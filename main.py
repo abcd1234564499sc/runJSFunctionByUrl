aed4dad60988036e0a764fa0764b0aaecfe8e7b5
abcd1234564499sc <mnj1254@126.com>
2022-03-18 13:33:50 +0800

    v1.0
    上传项目

---
+++ main.py
@@ -0,0 +1,242 @@
+#!/usr/bin/env python
+# coding=utf-8
+import sys
+
+import openpyxl as oxl
+from PyQt5.QtCore import QUrl
+from PyQt5.QtGui import QTextCursor
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtWidgets import QMainWindow, QApplication, QHeaderView
+
+import myUtils
+from ui.mainForm import Ui_MainWindow
+
+
+class Main(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+        self.checkedList = []
+        self.inputFormattedList = []
+        self.updateInputFormat()
+        self.resetJsFunctionITextEdit()
+        self.browser = QWebEngineView()
+        # 设置结果表头
+        self.resultHeaderList = ["序号", "输入", "输出"]
+        self.resultTable.setColumnCount(len(self.resultHeaderList))
+        self.resultTable.setHorizontalHeaderLabels(self.resultHeaderList)
+        self.resultTable.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.resultTable.horizontalHeader().setSectionResizeMode(0, QHeaderView.Interactive)
+        self.resultTable.verticalHeader().setVisible(False)
+
+    def updateInputFormat(self, checkState=0):
+        # 遍历4个输入框的复选框，根据选择情况构建inputFormatLineEdit的值
+        formatStr = "{"
+        checkBoxList = [self.input1CheckBox, self.input2CheckBox, self.input3CheckBox, self.input4CheckBox]
+        checkedList = []
+        for tmpIndex, nowCheckBox in enumerate(checkBoxList):
+            if nowCheckBox.isChecked():
+                formatStr = formatStr + "\"输入值{0}\":\"$%$输入值{0}$%$\",".format(tmpIndex + 1)
+                checkedList.append(1)
+            else:
+                checkedList.append(0)
+        if formatStr[-1] == ",":
+            formatStr = formatStr[:-1]
+        else:
+            pass
+        formatStr = formatStr + "}"
+        self.inputFormatLineEdit.setText(formatStr)
+        self.checkedList = checkedList
+
+    # 判断是否完成输入
+    def checkInput(self):
+        checkFlag = True
+        checkStr = ""
+        nowUrl = self.urlLineEdit.text()
+        if nowUrl == "" and checkFlag:
+            checkFlag = False
+            checkStr = "请输入URL"
+        else:
+            pass
+        if not myUtils.checkIfUrl(nowUrl) and checkFlag:
+            checkFlag = False
+            checkStr = "输入的URL不符合规范"
+        else:
+            pass
+        if sum(self.checkedList) == 0 and checkFlag:
+            checkFlag = False
+            checkStr = "请至少启用一个输入"
+        else:
+            pass
+        if self.jsFunTextEdit.toPlainText() == "" and checkFlag:
+            checkFlag = False
+            checkStr = "js处理函数不能留空"
+        else:
+            pass
+        self.inputFormattedList = self.getInput()
+        if len(self.inputFormattedList) == 0 and checkFlag:
+            checkFlag = False
+            checkStr = "输入为空"
+        else:
+            pass
+        return checkFlag, checkStr
+
+    # 写日志，logType：0代表流程日志，1代表错误日志
+    def writeLog(self, logStr, logType=0):
+        # 获取当前时间
+        nowSeconed = myUtils.getNowSeconed()
+        finalLog = "[{0}] {1}".format(nowSeconed, logStr)
+        if logType == 0:
+            self.normalLogTextEdit.moveCursor(QTextCursor.End)
+            self.normalLogTextEdit.append(finalLog)
+            self.normalLogTextEdit.moveCursor(QTextCursor.End)
+        else:
+            self.errorLogTextEdit.moveCursor(QTextCursor.End)
+            self.errorLogTextEdit.append(finalLog)
+            self.errorLogTextEdit.moveCursor(QTextCursor.End)
+
+    def resetJsFunctionITextEdit(self):
+        functionModelStr = ''' function process(nowInputVal){\n    var nowOutputVal = "";\n    nowInputVal=String(nowInputVal);\n    // 逻辑代码\n    nowOutputVal=nowInputVal;\n    \n    return nowOutputVal;\n}'''
+        self.jsFunTextEdit.setText(functionModelStr)
+
+    def getInput(self):
+        # 遍历输入
+        inputTextEditList = []
+        inputTextEditList.append(self.input1TextEdit)
+        inputTextEditList.append(self.input2TextEdit)
+        inputTextEditList.append(self.input3TextEdit)
+        inputTextEditList.append(self.input4TextEdit)
+        inputResultList = []
+        for tmpIndex, tmpCheckFlag in enumerate(self.checkedList):
+            if tmpCheckFlag == 0:
+                nowInputList = [""]
+            else:
+                nowInputList = inputTextEditList[tmpIndex].toPlainText().replace("\r\n", "\n").split("\n")
+            inputResultList.append(nowInputList)
+        nowInputFormat = self.inputFormatLineEdit.text()
+        nowInputFormat = nowInputFormat.replace("{", "{{")
+        nowInputFormat = nowInputFormat.replace("}", "}}")
+        for tmpIndex, tmpVal in enumerate(self.checkedList):
+            nowInputFormat = nowInputFormat.replace("$%$输入值{0}$%$".format(tmpIndex + 1), "{" + str(tmpIndex) + "}")
+        maxCoordinate = [len(a) - 1 for a in inputResultList]
+        nowCoordinate = [0 for a in inputResultList]
+        resultList = []
+
+        ifAddFlag = True
+        while ifAddFlag:
+            nowResult = ""
+            nowInputItemList = []
+            for tmpIndex in range(0, len(inputResultList)):
+                nowInputItemList.append(inputResultList[tmpIndex][nowCoordinate[tmpIndex]])
+            nowResult = nowInputFormat.format(*nowInputItemList)
+            if nowResult != "":
+                resultList.append(nowResult)
+            else:
+                pass
+            ifAddFlag, nowCoordinate = self.coordinatePlus(nowCoordinate, maxCoordinate)
+        return resultList
+
+    def coordinatePlus(self, nowCoordinate, maxCoordinate):
+        ifAddFlag = False
+        nowAddIndex = -1
+        while not ifAddFlag:
+            if len(nowCoordinate) + nowAddIndex == -1:
+                ifAddFlag = False
+                break
+            else:
+                nowCoordinateItem = nowCoordinate[nowAddIndex]
+                nowMaxCoordinateItem = maxCoordinate[nowAddIndex]
+                if nowCoordinateItem == nowMaxCoordinateItem:
+                    tmpAddIndex = nowAddIndex
+                    while tmpAddIndex != 0:
+                        nowCoordinate[tmpAddIndex] = 0
+                        tmpAddIndex = tmpAddIndex + 1
+                    nowAddIndex = nowAddIndex - 1
+                else:
+                    nowCoordinate[nowAddIndex] = nowCoordinate[nowAddIndex] + 1
+                    ifAddFlag = True
+        return ifAddFlag, nowCoordinate
+
+    def runJs(self):
+        checkFlag, checkStr = self.checkInput()
+        if not checkFlag:
+            self.writeLog("运行发生异常，请确认错误日志")
+            self.writeLog(checkStr, 1)
+        else:
+            url = self.urlLineEdit.text()
+            while url[-1] == "/":
+                url = url[:-1]
+            self.writeLog("开始执行自定义JS函数")
+            self.browser.load(QUrl(url))
+            nowJsFunction = self.jsFunTextEdit.toPlainText()
+            nowJsFunction = nowJsFunction.replace("\"", "\\\"")
+            nowJsFunction = nowJsFunction.replace("\n", "\\n\\\n")
+            nowJsStr = '''var script = document.createElement("script");
+script.type = "text/javascript";
+script.text = "{0}";
+document.body.appendChild(script);'''.format(nowJsFunction)
+            self.browser.page().runJavaScript(nowJsStr)
+            myUtils.clearTalbe(self.resultTable)
+            for tmpIndex, tmpInput in enumerate(self.inputFormattedList):
+                nowInput = tmpInput
+                self.writeLog("使用输入值({1}/{2})：{0}".format(nowInput, tmpIndex + 1, len(self.inputFormattedList)))
+                nowInputFormat = nowInput.replace("\"", "\\\"")
+                nowJsStr = "process(\"{0}\")".format(nowInputFormat)
+                self.browser.page().runJavaScript(nowJsStr, self.jsCallBack)
+
+            self.writeLog("执行完成，结果请前往输出tab查看")
+
+    def jsCallBack(self, result):
+        nowTableRowCount = self.resultTable.rowCount()
+        self.resultTable.insertRow(nowTableRowCount)
+        self.resultTable.setItem(nowTableRowCount, 0, myUtils.createTableItem(str(nowTableRowCount + 1)))
+        self.resultTable.setItem(nowTableRowCount, 1,
+                                 myUtils.createTableItem(self.inputFormattedList[nowTableRowCount]))
+        self.resultTable.setItem(nowTableRowCount, 2, myUtils.createTableItem(result))
+
+    def closeEvent(self, event):
+        self.browser.close()
+        event.accept()
+
+    def exportResult(self):
+        nowTable = self.resultTable
+        nowResultCount = nowTable.rowCount()
+        if nowResultCount == 0:
+            self.writeLog("当前无可导出数据")
+            return
+        filename = "导出文件 " + myUtils.getNowSeconed()
+        filename = myUtils.updateFileNameStr(filename)
+        # 创建一个excell文件对象
+        wb = oxl.Workbook()
+        # 创建URL扫描结果子表
+        ws = wb.active
+        ws.title = "JS函数执行结果"
+        # 创建表头
+        myUtils.writeExcellHead(ws, self.resultHeaderList)
+
+        # 遍历当前结果
+        for rowIndex in range(nowResultCount):
+            # 获取当前行的值
+            nowIndex = nowTable.item(rowIndex, 0).text()
+            nowInput = nowTable.item(rowIndex, 1).text()
+            nowResultStr = nowTable.item(rowIndex, 2).text()
+
+            # 将值写入excell对象
+            myUtils.writeExcellCell(ws, rowIndex + 2, 1, nowIndex, 0, True)
+            myUtils.writeExcellCell(ws, rowIndex + 2, 2, nowInput, 0, True)
+            myUtils.writeExcellCell(ws, rowIndex + 2, 3, nowResultStr, 0, True)
+            myUtils.writeExcellSpaceCell(ws, rowIndex + 2, 4)
+
+        # 设置列宽
+        colWidthArr = [7, 20, 60]
+        myUtils.setExcellColWidth(ws, colWidthArr)
+        # 保存文件
+        myUtils.saveExcell(wb, saveName=filename)
+        self.writeLog("成功保存文件：{0}.xlsx 至当前文件夹".format(filename))
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    mainForm = Main()
+    mainForm.show()
+    sys.exit(app.exec_())
---
+++ myUtils.py
@@ -0,0 +1,307 @@
+#!/usr/bin/env python
+# coding=utf-8
+import datetime
+import os
+import re
+import shutil
+
+import requests
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QTableWidgetItem
+from bs4 import BeautifulSoup
+import openpyxl as oxl
+
+# 全局变量区域
+borderNumDic = {-1: None, 0: "thin"}
+
+# 访问URL,type表示请求类型，0为GET，1为POST，2为PUT
+# 返回值类型如下：
+# {
+# "url":传入URL,
+# "resultStr":访问结果字符串,
+# "checkFlag":标志是否访问成功的布尔类型变量,
+# "title":访问成功时的页面标题,
+# "pageContent":访问成功时的页面源码，
+# "status":访问的响应码，
+# "requestSeconed":访问耗时，单位为秒
+# }
+from openpyxl.styles import Border, Side, Font, PatternFill
+
+
+def requestsUrl(url, cookie={}, header={}, data={}, files=None, type=0, reqTimeout=10, readTimeout=10,
+                allow_redirects=False, session=None, proxy=""):
+    proxies = None
+    if proxy != "":
+        proxies = {
+            'http': 'http://' + proxy,
+            'https': 'https://' + proxy
+        }
+    else:
+        pass
+    if session is None:
+        session = requests.session()
+    else:
+        pass
+    resDic = {}
+    url = url.strip()
+    url = url.strip()
+
+    resultStr = ""
+    checkedFlag = False
+    status = ""
+    title = ""
+    reContent = ""
+    totalSeconed = 0
+    timeout = (reqTimeout, readTimeout)
+    header = header if header != {} else {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
+    }
+    try:
+        if type == 0:
+            response = session.get(url, headers=header, verify=False, cookies=cookie, timeout=timeout,
+                                   allow_redirects=allow_redirects, proxies=proxies)
+        elif type == 1:
+            response = session.post(url, headers=header, verify=False, cookies=cookie, data=data, files=files,
+                                    timeout=timeout, allow_redirects=allow_redirects, proxies=proxies)
+        elif type == 2:
+            response = session.put(url, headers=header, verify=False, cookies=cookie, data=data, files=files,
+                                   timeout=timeout, allow_redirects=allow_redirects, proxies=proxies)
+        else:
+            pass
+        status = response.status_code
+        totalSeconed = response.elapsed.total_seconds()
+        if str(status)[0] == "2" or str(status)[0] == "3":
+            # 获得页面编码
+            pageEncoding = response.apparent_encoding
+            # 设置页面编码
+            response.encoding = pageEncoding
+            # 获得页面内容
+            reContent = response.text
+            soup = BeautifulSoup(reContent, "lxml")
+            title = "成功访问，但无法获得标题" if not soup.title else soup.title.string
+            resultStr = "验证成功，标题为：{0}".format(title)
+            checkedFlag = True
+        else:
+            resultStr = "验证失败，状态码为{0}".format(status)
+            # 获得页面编码
+            pageEncoding = response.apparent_encoding
+            # 设置页面编码
+            response.encoding = pageEncoding
+            # 获得页面内容
+            reContent = response.text
+            checkedFlag = False
+    except Exception as e:
+        resultStr = str(e)
+        checkedFlag = False
+
+    # 构建返回结果
+    resDic["url"] = url
+    resDic["resultStr"] = resultStr
+    resDic["checkFlag"] = checkedFlag
+    resDic["title"] = title
+    resDic["status"] = status
+    resDic["pageContent"] = reContent
+    resDic["requestSeconed"] = totalSeconed
+    return resDic
+
+
+# 获得精确到秒的当前时间
+def getNowSeconed():
+    formatStr = "%Y-%m-%d %H:%M:%S"
+    nowDate = datetime.datetime.now()
+    nowDateStr = nowDate.strftime(formatStr)
+    return nowDateStr
+
+
+# 将传入的字符串修改为符合windows文件名规范的字符串
+def updateFileNameStr(oriStr):
+    resultStr = oriStr
+    # 替换换行符
+    resultStr = resultStr.replace("\r\n", "\n").replace("\n", "")
+    # 将违法字符替换为下划线
+    notAllowCharList = ["\\", "/", ":", "*", "?", "\"", "<", ">", "|"]
+    for nowNotAllowChar in notAllowCharList:
+        resultStr = resultStr.replace(nowNotAllowChar, "_")
+    return resultStr
+
+
+# 在本目录创建一个文件夹,ifDelete 表示如果已经存在该文件夹是否会删除原有文件夹并重新创建
+# 返回一个布尔变量，表示是否创建了文件夹
+def createFolderHere(folderName, ifDelete=True):
+    basePath = os.getcwd()
+    ifCreate = True
+    if os.path.exists(folderName):
+        if ifDelete:
+            shutil.rmtree(basePath + "\\" + folderName)
+            os.makedirs(basePath + "\\" + folderName)
+        else:
+            ifCreate = False
+    else:
+        # 不存在文件夹，创建文件夹
+        os.makedirs(basePath + "\\" + folderName)
+
+    return ifCreate
+
+
+def checkIfUrl(urlStr):
+    checkFlag = True
+    urlRegex = re.compile(
+        r'^(?:http|ftp)s?://'  # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+        r'localhost|'  # localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+        r'(?::\d+)?'  # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    checkFlag = urlRegex.match(urlStr)
+    return checkFlag
+
+# 传入一个QTableWidget对象，清空这个表格
+def clearTalbe(tableObject):
+    while tableObject.rowCount() != 0:
+        tableObject.removeRow(tableObject.rowCount() - 1)
+
+# 创建一个QTableWidgetItem对象，传入文本参数text，以及verAlign和horAlign两个整形参数控制文本位置
+# verAlign的值含义如下：-1 靠左、0 居中、1 靠右
+# horAlign的值含义如下：-1 靠下、0 居中、1 靠上
+def createTableItem(text, verAlign=0, horAlign=0):
+    tmpItem = QTableWidgetItem(str(text))
+    verAlignDic = {-1: Qt.AlignLeft, 0: Qt.AlignVCenter, 1: Qt.AlignRight}
+    horAlignDic = {-1: Qt.AlignBottom, 0: Qt.AlignHCenter, 1: Qt.AlignTop}
+    tmpItem.setTextAlignment(verAlignDic[verAlign] | horAlignDic[horAlign])
+    return tmpItem
+
+# 删除指定路径的文件,传入一个绝对路径,返回一个布尔变量以及一个字符串变量，
+# 布尔变量为True表示是否删除成功,若为False则字符串变量中写入错误信息
+def deleteFile(filePath):
+    deleteFlag = True
+    reStr = ""
+    if os.path.exists(filePath):
+        try:
+            os.remove(filePath)
+        except Exception as ex:
+            reStr = "删除失败，失败信息为：{0}".format(ex)
+            deleteFlag = False
+    else:
+        reStr = "未找到指定路径的文件"
+        deleteFlag = False
+    return deleteFlag, reStr
+
+# 获得excell的常用样式
+def getExcellStyleDic():
+    styleDic = {}
+
+    # 单线边框
+    thinBorder = Border(left=Side(border_style='thin', color='000000'),
+                        right=Side(border_style='thin', color='000000'),
+                        top=Side(border_style='thin', color='000000'),
+                        bottom=Side(border_style='thin', color='000000'))
+
+    # 文字居中
+    alignStyle = oxl.styles.Alignment(horizontal='center', vertical='center')
+    leftStyle = oxl.styles.Alignment(horizontal='left', vertical='center')
+    rightStyle = oxl.styles.Alignment(horizontal='right', vertical='center')
+
+    # 加粗字体
+    boldFont = Font(bold=True)
+    hyperLinkFont = Font(color='0000FF')
+    underLineFont = Font(underline='single')
+
+    styleDic["thin"] = thinBorder
+    styleDic["align"] = alignStyle
+    styleDic["bold"] = boldFont
+    styleDic["left"] = leftStyle
+    styleDic["right"] = rightStyle
+    styleDic["link"] = hyperLinkFont
+    styleDic["underLine"] = underLineFont
+    return styleDic
+
+
+# 写入一个标准的excell表头（居中，单线框，加粗）
+def writeExcellHead(ws, headArr):
+    # 获得常用样式
+    styleDic = getExcellStyleDic()
+    # 写入表头
+    for index, head in enumerate(headArr):
+        ws.cell(row=1, column=index + 1).value = head
+        ws.cell(row=1, column=index + 1).border = styleDic["thin"]
+        ws.cell(row=1, column=index + 1).alignment = styleDic["align"]
+        ws.cell(row=1, column=index + 1).font = styleDic["bold"]
+    return ws
+
+
+# 写入一个内容单元格
+# borderNum表示该单元格的边框对象，其值可查询全局变量styleDic
+# ifAlign是一个boolean对象，True表示居中
+# hyperLink表示该单元格指向的链接，默认为None，表示不指向任何链接
+# fgColor表示该单元格的背景颜色，为一个RGB16进制字符串，默认为“FFFFFF”（白色）
+# otherAlign表示当ifAlign为False时指定的其他对齐方式，是一个数字型变量，默认为None，当其为0时表示左对齐，1为右对齐
+def writeExcellCell(ws, row, column, value, borderNum, ifAlign, hyperLink=None, fgColor="FFFFFF", otherAlign=None):
+    # 获得常用样式
+    styleDic = getExcellStyleDic()
+    # 获得指定单元格
+    aimCell = ws.cell(row=row, column=column)
+    # 设置值
+    aimCell.value = value
+    # 设置边框
+    styleObjKey = borderNumDic[borderNum]
+    if not styleObjKey:
+        pass;
+    else:
+        styleObj = styleDic[styleObjKey]
+        aimCell.border = styleObj
+    # 设置居中
+    if ifAlign:
+        aimCell.alignment = styleDic["align"]
+    elif otherAlign is not None:
+        otherAlign = int(otherAlign)
+        if otherAlign == 0:
+            aimCell.alignment = styleDic["left"]
+        else:
+            aimCell.alignment = styleDic["right"]
+    else:
+        pass
+
+    # 设置超链接
+    if hyperLink:
+        # 写入超链接
+        aimCell.hyperlink = hyperLink
+        # 设置当前单元格字体颜色为深蓝色，并添加下划线
+        aimCell.font = styleDic["link"]
+    else:
+        pass
+
+    # 设置填充颜色
+    fill = PatternFill("solid", fgColor=fgColor)
+    aimCell.fill = fill
+
+    return ws
+
+
+# 写入一个空格单元格，防止上一列文本超出
+def writeExcellSpaceCell(ws, row, column):
+    # 设置值
+    ws.cell(row=row, column=column).value = " "
+
+    return ws
+
+
+# 设置excell的列宽
+def setExcellColWidth(ws, colWidthArr):
+    for colWidindex in range(len(colWidthArr)):
+        ws.column_dimensions[chr(ord("A") + colWidindex)].width = colWidthArr[colWidindex]
+
+    return ws
+
+
+# 保存excell文件
+def saveExcell(wb, saveName):
+    savePath = ""
+    # 处理传入的文件名
+    saveName = saveName.split(".")[0] + ".xlsx"
+    savePath = "{0}\\{1}".format(os.getcwd(), saveName)
+
+    # 检测当前目录下是否有该文件，如果有则清除以前保存文件
+    if os.path.exists(savePath):
+        deleteFile(savePath)
+    wb.save(savePath)
+    return True
