ac32e152fe6634393f04aa5de33bae8e39cfc53c
abcd1234564499sc <mnj1254@126.com>
2024-07-11 09:44:42 +0800

    v3.4
    修复因SSL证书验证不过导致的ERR_CERT_AUTHORITY_INVALID

--- ErrorSignalWebPageConsole.py
+++ ErrorSignalWebPageConsole.py
@@ -10,3 +10,14 @@ class ErrorSignalWebPageConsole(QWebEnginePage):
     def javaScriptConsoleMessage(self, level, msg, line, sourceID):
         if level == QWebEnginePage.ErrorMessageLevel:
             self.signal_console_error.emit(msg)
+            
+    def certificateError(self, error):
+        # If you want to ignore the certificates of certain pages
+        # then do something like
+        # if error.url() == QUrl("https://www.us.army.mil/"):
+        #     error.ignoreCertificateError()
+        #     return True
+        # return super().certificateError(error)
+
+        error.ignoreCertificateError()
+        return True
